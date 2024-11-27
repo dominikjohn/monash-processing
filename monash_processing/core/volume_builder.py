c49e919d7fc59c3196e0e078e4ecce24cd17ca75
dominikjohn <mail@dominikjohn.com>
2024-11-27 13:56:02 +1100

    find center shift

--- monash_processing/core/volume_builder.py
+++ monash_processing/core/volume_builder.py
@@ -79,13 +79,17 @@ class VolumeBuilder:
         detector_cols = projection_slices.shape[1]
 
         if is_stitched:
-            vol_geom = astra.create_vol_geom(int(detector_cols * 1.5), int(detector_cols * 1.5))
-            print('Volume geometry:', vol_geom)
+            vol_geom = astra.create_vol_geom(int(detector_cols * 1.7), int(detector_cols * 1.7))
         else:
             vol_geom = astra.create_vol_geom(detector_cols, detector_cols)
 
+        scaling_factor = 1e6
+        source_distance = 21.5 * scaling_factor
+        detector_distance = 0.158 * scaling_factor
+        pixel_size = 1.444e-6 * scaling_factor
+
         # Create projection geometry with center shift
-        proj_geom = astra.create_proj_geom('parallel', pixel_size, detector_cols, angles)
+        proj_geom = astra.create_proj_geom('fanflat', pixel_size, detector_cols, angles, source_distance, detector_distance)
 
         # Create sinogram
         sino_id = astra.data2d.create('-sino', proj_geom, projection_slices)
