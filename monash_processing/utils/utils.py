748f8ea8d9e648efada3cb0c71a689cdf19d5e25
dominikjohn <mail@dominikjohn.com>
2024-11-28 11:11:31 +1100

    find center shift

--- monash_processing/utils/utils.py
+++ monash_processing/utils/utils.py
@@ -6,20 +6,6 @@ from scipy.ndimage import shift as scipy_shift
 class Utils:
 
     @staticmethod
-    def perform_bad_pixel_correction(im, dead_pix_thl):
-        if dead_pix_thl > 0:
-            med_im = cv2.medianBlur(im, 5)
-            filter_im = (im / med_im)
-
-            # Check for both too-dark and too-bright pixels
-            lower_percentile = np.percentile(filter_im, dead_pix_thl * 100)
-            upper_percentile = np.percentile(filter_im, (1 - dead_pix_thl) * 100)
-
-            mask = (filter_im < lower_percentile) | (filter_im > upper_percentile)
-            im[mask] = med_im[mask]
-        return im
-
-    @staticmethod
     def select_areas(image):
         im = cv2.imread(image)
 
