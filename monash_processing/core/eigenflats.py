18bfd6de94181247420836d89b1a2767058badd9
dominikjohn <mail@dominikjohn.com>
2024-11-28 11:34:22 +1100

    find center shift

--- monash_processing/core/eigenflats.py
+++ monash_processing/core/eigenflats.py
@@ -41,25 +41,25 @@ class EigenflatManager:
         Can perform dead pixel correction
 
         """
-im = projection - darkcurrent
-im[im <= 0] = 1
+        im = projection - darkcurrent
+        im[im <= 0] = 1
 
-im = Utils.perform_bad_pixel_correction(im, dead_pix_thl)
+        im = Utils.perform_bad_pixel_correction(im, dead_pix_thl)
 
-imc = cv2.GaussianBlur(im / mean_flats[step], (0, 0), 3)
-m = nd.binary_erosion(np.abs(imc - 1) < 0.03, iterations=20)
+        imc = cv2.GaussianBlur(im / mean_flats[step], (0, 0), 3)
+        m = nd.binary_erosion(np.abs(imc - 1) < 0.03, iterations=20)
 
-# use predefined background
-#m = np.zeros_like(im, dtype=bool)
-#m[area] = 1
+        # use predefined background
+        #m = np.zeros_like(im, dtype=bool)
+        #m[area] = 1
 
-# calculate weights of EFs using a lstsq min in the masked area
-sh = eigenflats[step, 0][m].shape
-ef = eigenflats[step][:, m].reshape(eigenflats.shape[1], sh[0])
-sm = (im[m] - mean_flats[step][m]).ravel()
-res = scipy.optimize.lsq_linear(np.swapaxes(ef, 0, 1), sm)
+        # calculate weights of EFs using a lstsq min in the masked area
+        sh = eigenflats[step, 0][m].shape
+        ef = eigenflats[step][:, m].reshape(eigenflats.shape[1], sh[0])
+        sm = (im[m] - mean_flats[step][m]).ravel()
+        res = scipy.optimize.lsq_linear(np.swapaxes(ef, 0, 1), sm)
 
-# generate flat as weighted sum of EFs and MF
-mflat = np.sum(res['x'][:, None, None] * eigenflats[step], axis=0) + mean_flats[step]
+        # generate flat as weighted sum of EFs and MF
+        mflat = np.sum(res['x'][:, None, None] * eigenflats[step], axis=0) + mean_flats[step]
 
-return im, mflat, res['x']
+        return im, mflat, res['x']
