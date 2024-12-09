82396846c6132fb7a63ec40531c52289f5894999
dominikjohn <mail@dominikjohn.com>
2024-12-09 16:21:08 +1100

    clean up volume builder

--- monash_processing/core/volume_builder.py
+++ monash_processing/core/volume_builder.py
@@ -111,6 +111,7 @@ class VolumeBuilder:
             ig = ag.get_ImageGeometry()
             ig.voxel_num_x = (int)(ig.voxel_num_x + 2 * center_shift)
             ig.voxel_num_y = (int)(ig.voxel_num_y + 2 * center_shift)
+            ig.center_x = ag.config.system.rotation_axis._position[0]
         else:
             ig = ag.get_ImageGeometry()
         return ig
